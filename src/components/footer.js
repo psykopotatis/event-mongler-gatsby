2bc6435f848c7fc348a3c1ee529d422bab9d1caa
ingemar.finn <ingemar.finn@king.com>
2023-09-30 14:09:26 +0200

    Add bootstrap header, footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,13 @@ import * as React from "react"
 
 const Footer = () => {
     return (
-        <div>
-            <p>footer</p>
-        </div>
+        <footer className="py-3 my-4">
+            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
+            </ul>
+            <p className="text-center text-body-secondary">Senast uppdaterad .... Tipsa gärna om fler
+                fester/klubbar på <a href="mailto:technofest01@gmail.com">technofest01@gmail.com</a></p>
+        </footer>
     )
 }
 
