f8b5b193d5f7e81db7a54d21e9e78942d5380fd2
ingemar.finn <ingemar.finn@king.com>
2023-10-01 09:16:00 +0200

    Remove old events code

--- src/pages/events.js
+++ src/pages/events.js
@@ -20,35 +20,23 @@ const EventsList = ({eventsData}) => {
                         <ul className="list-group list-group-flush">
                             {eventsData?.edges?.map(({node}, index) => (
                                 <li className="list-group-item">
-                                <div key={index}>
-                                    <div>{node.formatted_start_date}</div>
-                                    <div className="event-url">
-                                        <b><a href="{node.url}" target="_blank">{node.title}</a></b>
+                                    <div key={index}>
+                                        <div>{node.formatted_start_date}</div>
+                                        <div className="event-url">
+                                            <b><a href="{node.url}" target="_blank">{node.title}</a></b>
+                                        </div>
+                                        <a href={node.url} target="_blank" rel="noreferrer">
+                                            {node.downloadedImages && (
+                                                <GatsbyImage image={getImage(node.downloadedImages)} alt={node.title}/>
+                                            )}
+                                        </a>
                                     </div>
-                                    <a href={node.url} target="_blank" rel="noreferrer">
-                                        {node.downloadedImages && (
-                                            <GatsbyImage image={getImage(node.downloadedImages)} alt={node.title}/>
-                                        )}
-                                    </a>
-                                </div>
                                 </li>
                             ))}
                         </ul>
                     </div>
 
-
                 </div>
-                {eventsData?.edges?.map(({node}, index) => (
-                    <div key={index}>
-                        <h2>{node.title}</h2>
-                        <p>{node.formatted_start_date}</p>
-                        <a href={node.url} target="_blank" rel="noreferrer">
-                            {node.downloadedImages && (
-                                <GatsbyImage image={getImage(node.downloadedImages)} alt={node.title}/>
-                            )}
-                        </a>
-                    </div>
-                ))}
             </div>
         </div>
     )
