4642c225cebd5863c1553044bf4b92032242e3cf
ingemar.finn <ingemar.finn@king.com>
2023-11-09 09:34:58 +0100

    White footer color

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,14 @@
 import * as React from "react"
+import './footer.css'
 
 const Footer = () => {
     return (
-        <footer className="py-3 my-4">
+        <footer className="py-3 my-4 footer">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
             </ul>
             <p className="text-center text-body-secondary">Senast uppdaterad .... Tipsa gärna om fler
-                fester/klubbar på <a href="mailto:technofest01@gmail.com">technofest01@gmail.com</a></p>
+                fester/klubbar på <a href="mailto:psykopotatis@gmail.com">psykopotatis@gmail.com</a></p>
         </footer>
     )
 }
