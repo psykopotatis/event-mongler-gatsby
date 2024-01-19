56a2372915634ba7d0c4b95637191e5d5508f743
ingemar.finn <ingemar.finn@king.com>
2024-01-19 21:23:41 +0100

    Add last updated date

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,21 @@ import * as React from "react"
 import './footer.css'
 
 const Footer = () => {
+    // Get current date and time
+    const now = new Date();
+    // Format the date and time
+    const formattedDate = now.toISOString().substring(0, 16).replace('T', ' ');
+
+
     return (
         <footer className="py-3 my-4 footer">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
             </ul>
-            <p className="text-center text-body-secondary">Senast uppdaterad .... Tipsa gärna om fler
-                fester/klubbar på <a href="mailto:psykopotatis@gmail.com">psykopotatis@gmail.com</a></p>
+            <p className="text-center text-body-secondary">
+                Tipsa gärna om fler fester/klubbar  <a href="mailto:psykopotatis@gmail.com">psykopotatis@gmail.com</a> <br/>
+                Senast uppdaterad: {formattedDate}
+            </p>
         </footer>
     )
 }
